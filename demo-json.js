269b4b416f31a8721798fb3ecc304ef59599b96b
roshan rijal <rijalroshan94@gmail.com>
2020-04-27 11:38:49 +0545

    file read write exaples

---
+++ demo-json.js
@@ -0,0 +1,41 @@
+var fs = require("fs");
+var array = [];
+array = JSON.parse(fs.readFileSync("user.json", { encoding: "utf-8" }));
+//todo : check username already exist or not
+//if not then push to array else
+array.push({ username: "username", password: "password", age: 14 });
+fs.writeFileSync("user.json", JSON.stringify(array), "utf-8");
+
+fs.writeFile("user.json", JSON.stringify(array), "utf-8", (err)=>{
+
+});
+
+console.log("registered");
+ar = fs.readFileSync("user.json", { encoding: "utf-8" });
+console.log(ar);
+array = JSON.parse(ar);
+array.forEach((item) => {
+  if (item.username === "username" && item.password === "password") {
+    console.log("logged in");
+    return;
+  }
+});
+
+function registerUser(usern, pas, callback) {
+  callback({ "message": "succ" });
+}
+
+
+array.forEach((item) => {
+  console.log(item);
+});
+
+array.map((item) => {
+  item.age = item.age + 1;
+  return item;
+});
+
+ar = array.filter((item) => {
+  return item.age % 2 == 0;
+});
+console.log(ar);
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var fs = require("fs");
-
+var userProfile = require('./demo-json.js')
 var app = express();
 app.use(bodyParser.json());
 app.get("/hello", function (req, res) {
   res.send("sdsdds");
 });
+
+
+
 
 app.post("/login", function (req, res) {
   console.log(req);
@@ -26,16 +29,21 @@ app.post("/register", function (req, res) {
   var username = req.body.username;
   var password = req.body.password;
   var data = username + "," + password + "\n";
-  fs.writeFile("users.txt", data, "utf-8", function (err) {
-    if (!err) {
-      res.send({
-        message: "registered successfully",
-      });
-    } else {
-      res.send({
-        message: "unable to register",
-      });
-    }
+  userProfile.registerUser(username, password, (result) => {
+    res.send(result)
+  })
+
+
+  // fs.writeFile("users.txt", data, "utf-8", function (err) {
+  //   if (!err) {
+  //     res.send({
+  //       message: "registered successfully",
+  //     });
+  //   } else {
+  //     res.send({
+  //       message: "unable to register",
+  //     });
+  //   }
   });
   //save to file
 });
